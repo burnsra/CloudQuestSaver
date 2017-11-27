f6e76a7ac6cb06741a79ba353a2a86073a4642b4
Robert Burns <burnsra@gmail.com>
2017-11-27 14:47:04 -0500

    Addition of WebKit to the ScreenSaver

--- ScreenSaverTemplate/ScreenSaverTemplateView.m
+++ ScreenSaverTemplate/ScreenSaverTemplateView.m
@@ -7,14 +7,18 @@
 //
 
 #import "ScreenSaverTemplateView.h"
+#import <WebKit/WebKit.h>
 
 @implementation ScreenSaverTemplateView
+
+WebView *_webView;
+NSString *_url;
 
 - (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
 {
     self = [super initWithFrame:frame isPreview:isPreview];
     if (self) {
-        [self setAnimationTimeInterval:1/30.0];
+        [self initialize];
     }
     return self;
 }
@@ -22,11 +26,17 @@
 - (void)startAnimation
 {
     [super startAnimation];
+    if (self) {
+        [self loadWebView];
+    }
 }
 
 - (void)stopAnimation
 {
     [super stopAnimation];
+    if (self) {
+        [self unloadWebView];
+    }
 }
 
 - (void)drawRect:(NSRect)rect
@@ -47,6 +57,40 @@
 - (NSWindow*)configureSheet
 {
     return nil;
+}
+
+- (void)configureWebUrl
+{
+    _url = [NSString stringWithFormat:@"file://%@/html/index.html", [[NSBundle bundleForClass:[self class]] resourcePath]];
+}
+
+- (void)configureWebView
+{
+    _webView = [[WebView alloc] initWithFrame:[self bounds]];
+    [_webView setShouldUpdateWhileOffscreen:YES];
+    [_webView setAutoresizingMask:NSViewWidthSizable|NSViewHeightSizable];
+    [_webView setAutoresizesSubviews:YES];
+    [_webView setDrawsBackground:NO];
+    [_webView setMaintainsBackForwardList:NO];
+    [self addSubview:_webView];
+}
+
+- (void)initialize
+{
+    [self configureWebUrl];
+    [self configureWebView];
+}
+
+- (void)loadWebView
+{
+    [_webView setMainFrameURL:[_url stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]]];
+    [_webView reloadFromOrigin:nil];
+}
+
+- (void)unloadWebView
+{
+    [_webView removeFromSuperview];
+    [_webView close];
 }
 
 @end
